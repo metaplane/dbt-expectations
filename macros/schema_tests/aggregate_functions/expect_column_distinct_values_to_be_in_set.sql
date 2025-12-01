ad5f82362be2cfe02e9bdbd5684c2cac5eee1bfc
vs255034 <113147289+varun-sharma_teradata@users.noreply.github.com>
2025-12-01 17:52:19 +0530

    changes for column value macros

--- macros/schema_tests/aggregate_functions/expect_column_distinct_values_to_be_in_set.sql
+++ macros/schema_tests/aggregate_functions/expect_column_distinct_values_to_be_in_set.sql
@@ -5,6 +5,19 @@
                                                      row_condition=None
                                                      ) %}
 
+    {{ adapter.dispatch('test_expect_column_distinct_values_to_be_in_set', 'dbt_expectations') (
+            model, column_name, value_set, quote_values, row_condition
+        ) }}
+
+{% endtest %}
+
+{% macro default__test_expect_column_distinct_values_to_be_in_set(model,
+                                                                  column_name,
+                                                                  value_set,
+                                                                  quote_values,
+                                                                  row_condition
+                                                                  ) %}
+
 with all_values as (
 
     select distinct
@@ -52,4 +65,61 @@ validation_errors as (
 select *
 from validation_errors
 
-{% endtest %}
+{% endmacro %}
+
+{% macro teradata__test_expect_column_distinct_values_to_be_in_set(model,
+                                                                   column_name,
+                                                                   value_set,
+                                                                   quote_values,
+                                                                   row_condition
+                                                                   ) %}
+
+with all_values as (
+
+    select distinct
+        {{ column_name }} as value_field
+
+    from {{ model }}
+    {% if row_condition %}
+    where {{ row_condition }}
+    {% endif %}
+
+),
+set_values as (
+
+    {% for value in value_set -%}
+    select
+        {% if quote_values -%}
+        '{{ value }}'
+        {%- else -%}
+        {{ value }}
+        {%- endif %} as value_field
+    from SYS_CALENDAR.CALENDAR where day_of_calendar = 1
+    {% if not loop.last %}union all{% endif %}
+    {% endfor %}
+
+),
+unique_set_values as (
+
+    select distinct value_field
+    from
+        set_values
+
+),
+validation_errors as (
+    -- values from the model that are not in the set
+    select
+        v.value_field
+    from
+        all_values v
+        left join
+        unique_set_values s on v.value_field = s.value_field
+    where
+        s.value_field is null
+
+)
+
+select *
+from validation_errors
+
+{% endmacro %}
