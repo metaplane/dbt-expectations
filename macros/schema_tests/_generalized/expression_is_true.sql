234e8cc9f5b61dc77a3924c52ee61c54614f9b20
Tim B. <tim.bott@on-geo.de>
2026-06-09 14:55:28 +0200

    Work with 0 1 bits

--- macros/schema_tests/_generalized/expression_is_true.sql
+++ macros/schema_tests/_generalized/expression_is_true.sql
@@ -56,3 +56,42 @@ from validation_errors
 
 
 {% endmacro -%}
+
+{% macro sqlserver__expression_is_true(model, expression, test_condition, group_by_columns, row_condition) -%}
+with grouped_expression as (
+    select
+        {% if group_by_columns %}
+        {% for group_by_column in group_by_columns -%}
+        {{ group_by_column }} as col_{{ loop.index }},
+        {% endfor -%}
+        {% endif %}
+        {{ dbt_expectations.truth_expression(expression) }}
+    from {{ model }}
+     {%- if row_condition %}
+    where
+        {{ row_condition }}
+    {% endif %}
+    {% if group_by_columns %}
+    group by
+    {% for group_by_column in group_by_columns -%}
+        {{ group_by_column }}{% if not loop.last %},{% endif %}
+    {% endfor %}
+    {% endif %}
+
+),
+validation_errors as (
+
+    select
+        *
+    from
+        grouped_expression
+    where
+        not(expression =1)
+
+)
+
+select *
+from validation_errors
+
+
+{% endmacro -%}
